9267b5f1c156641ac900c9f50fb09a2632902b5d
Dilafruz Shukurova <dilafruzshukurova36@gmail.com>
2024-04-22 11:06:36 +0500

    dan

---
+++ index.js
@@ -0,0 +1,28 @@
+let word = prompt("hafta kunini  kiriting");
+
+if (word === "monday") {
+    alert("dushanba");
+}
+
+if (word === "tuesday") {
+    alert("seshanba");
+}
+if (word === "wednesday") {
+    alert("chorshanba");
+}
+
+if (word === "thursday") {
+    alert("payshanba");
+}
+
+if (word === "friday") {
+    alert("juma");
+}
+
+if (word === "saturday") {
+    alert("shanba");
+}
+
+if (word === "sunday") {
+    alert("yakshanba");
+}
